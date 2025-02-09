686f24f5551106f58e831def479040823de9b0fb
Sumya <sumyahoque2012@gmail.com>
2025-02-09 00:41:22 -0500

    second page is done

--- Frontend/popup/src/App.jsx
+++ Frontend/popup/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+
 import './App.css'
 import WatchList from './components/watchlist'
+import Model_Summary from './components/model';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <WatchList></WatchList>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/home" element={<WatchList></WatchList>}/>
+    <Route path="/model" element={<Model_Summary></Model_Summary>}></Route>
+    </Routes>
+    </BrowserRouter>
 
     </>
   )
