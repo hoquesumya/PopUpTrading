757461841b268809065a2eb080cf07efeb43ec16
Sumya <sumyahoque2012@gmail.com>
2025-02-09 06:13:38 -0500

    added api call

--- Frontend/popup/src/components/model.jsx
+++ Frontend/popup/src/components/model.jsx
@@ -1,16 +1,37 @@
 import React from "react"
 import "../styles/model.css"
 import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from "react";
 import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
+import axios from "axios";
 import Navbar from 'react-bootstrap/Navbar';
 import { Button } from "react-bootstrap";
+
 function Model_Summary(){
     const location = useLocation()
     const tick  = location.state
+    const [hasFetched, setHasFetched] = useState(false);
     const ticker = tick["ticker_type"]
-    
+    useEffect(() => {
+        const fetch_data = async () =>{
+            try {
+                const response = await axios.get("http://127.0.0.1:8000/Sentiment");
+                console.log("res",response)
+                setHasFetched(true)
+
+            }catch (err) {   
+                console.error("Error occurred:", err.response); // Log full error response
+                console.error("Error message:", err.message);
+                console.error("Error stack:", err.stack);
+            }
+        }
+       if (ticker == "SPY" && !hasFetched){
+            fetch_data()
+       }
+    }, [ticker,hasFetched]); 
     console.log(ticker)
+
     return(
         <>
         <div  className = {`${ticker}_model main_details`} >
@@ -24,7 +45,7 @@ function Model_Summary(){
                 </Nav>
         </Navbar>
         <div className="main_model_details"> 
-            {ticker}
+            {ticker == "SPY" && <>SPY</>}
         </div>
 
         </div>
